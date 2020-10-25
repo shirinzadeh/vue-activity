61509511d3a3ca730b9624c860d7ace09e33d3ef
Tahmasib Shirinzade <tshzade@gmail.com>
2020-10-25 16:03:59 +0400

    merging code from vue-goals folder

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App), //(App) yox, hansisa html code yazsaq browserde onu render edecek, yeni gosterecek. hal hazirda app.vue-daki kodlari render edir
+}).$mount('#app') //vue index.htmle mounting edir
